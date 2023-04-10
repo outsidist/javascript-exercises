8cee263a73388652c09637dccbb101cb1931ff79
Outsidist <michaelfeazey@gmail.com>
2023-04-10 19:25:43 +0100

    Solve for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  //To convert to celsius subtract 32, divide by 1.8
+  convertedToCelsius = ((parseFloat(deg) - 32) / 1.8).toFixed(1);
+  return parseFloat(convertedToCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  //To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
+  convertedToFahrenheit = ((parseFloat(deg)*1.8)+32).toFixed(1);
+  return parseFloat(convertedToFahrenheit);
 };
 
 // Do not edit below this line
