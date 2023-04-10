6f4652cbea1fec89fca4c03e5693945cc581e74b
Outsidist <michaelfeazey@gmail.com>
2023-04-10 21:21:02 +0100

    Solve for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (original) {
+    originalLowerWithoutSpaces = original.replace(/\s/g, "").replace(/\p{P}/gu, "").toLowerCase();
+    reversedLowerWithoutSpaces = originalLowerWithoutSpaces.split("").reverse().join("")
+    if (originalLowerWithoutSpaces === reversedLowerWithoutSpaces){
+        return true;
+    } else {
+        return false;
+    }
+/*
+take user input (String)
+format string as lower
+remove spaces from string, store as originalLowerWithoutSpaces
+reverse string (from 04 exercise) const reverseString = function(toReverse) {
+    return originalLowerWithoutSpaces.split("").reverse().join("")
+    store as reversedLowerWithoutSpaces
+check whether originalLowerWithoutSpaces and reversedLowerWithoutSpaces are identical
+if identical return true, else false
+*/
 };
 
 // Do not edit below this line
