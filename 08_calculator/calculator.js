2ef91e7d72fce60c86db10a520e492026123d248
Outsidist <michaelfeazey@gmail.com>
2023-04-10 21:07:11 +0100

    Solve for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(arg1,arg2) {
+	let added = arg1+arg2;
+  return added;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1,arg2) {
+  let subtracted = arg1-arg2;
+	return subtracted;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	let summed = 0
+  for (let i = 0; i<args.length; i++){
+    summed += args[i];
+  }
+  return summed;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let multiplied = args[0];
+  for (let i = 1; i < args.length; i++){
+    multiplied *= args[i];
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let powered = base ** exponent;
+  return powered;
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+  if (arg === 0 || arg === 1) {
+    return 1;
+  } else {
+	let factorialOutput = arg;
+  for (let i = arg-1; i > 0; i--){
+    factorialOutput *= i;
+  }
+  return factorialOutput;
+  }
 };
 
 // Do not edit below this line
